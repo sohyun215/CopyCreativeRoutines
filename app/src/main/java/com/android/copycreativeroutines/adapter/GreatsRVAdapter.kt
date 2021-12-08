69f5b90b3283e9833d975a756b46a21f5f4a9a4a
Jaehun-Jo <chojh922@gmail.com>
2021-12-09 04:26:27 +0900

    [FIX] GreatsFragment 이미지 로드 방식 변경,GreatDetail 자기소개 xml 수정

--- app/src/main/java/com/android/copycreativeroutines/adapter/GreatsRVAdapter.kt
+++ app/src/main/java/com/android/copycreativeroutines/adapter/GreatsRVAdapter.kt
@@ -1,9 +1,11 @@
 package com.android.copycreativeroutines.adapter
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.android.copycreativeroutines.R
 import com.android.copycreativeroutines.data.Great
 import com.android.copycreativeroutines.databinding.ItemGreatsListBinding
 import com.bumptech.glide.Glide
@@ -25,12 +27,22 @@ class GreatsRVAdapter : RecyclerView.Adapter<GreatsRVAdapter.GreatsViewHolder>()
 
     class GreatsViewHolder (private val binding : ItemGreatsListBinding) : RecyclerView.ViewHolder(binding.root){
         fun onBind(data : Great) {
-            Firebase.storage.reference.child(data.image).downloadUrl.addOnSuccessListener {
-                Glide.with(this.itemView.context)
-                    .load(it.toString())
+            // Firebase Storage에서 이미지를 갑져오는 방식이 너무 오래걸려서 프로젝트 내부 사진으로부터 가져옴
+            val imageName = data.image.chunked(data.image.length-4)[0]
+            val image = this.itemView.context.resources.getIdentifier(imageName, "drawable", this.itemView.context.packageName)
+            Log.d("testtt", image.toString())
+            Glide.with(this.itemView.context)
+                    .load(image)
                     .circleCrop()
                     .into(binding.ivGreatsImage)
-            }
+            
+            // Firbase Storage에서 이미지를 가져오는 방식
+//            Firebase.storage.reference.child(data.image).downloadUrl.addOnSuccessListener {
+//                Glide.with(this.itemView.context)
+//                    .load(it.toString())
+//                    .circleCrop()
+//                    .into(binding.ivGreatsImage)
+//            }
             binding.tvGreatName.text = data.name
         }
     }
