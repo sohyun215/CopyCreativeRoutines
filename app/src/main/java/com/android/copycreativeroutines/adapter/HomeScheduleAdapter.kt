c9f0e9d834b62da797a4e6a367508ea74ea0848b
KangSungBum <peter9706@naver.com>
2021-12-10 17:51:55 +0900

    bug fixed

--- app/src/main/java/com/android/copycreativeroutines/adapter/HomeScheduleAdapter.kt
+++ app/src/main/java/com/android/copycreativeroutines/adapter/HomeScheduleAdapter.kt
@@ -8,13 +8,17 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.android.copycreativeroutines.data.Great
 import com.android.copycreativeroutines.data.User
+import com.android.copycreativeroutines.databinding.FragmentHomeBinding
 import com.android.copycreativeroutines.databinding.ItemScheduleListBinding
 import com.android.copycreativeroutines.util.FBAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlin.coroutines.coroutineContext
+import kotlin.math.log
 
 class HomeScheduleAdapter() : RecyclerView.Adapter<HomeScheduleAdapter.ViewHolder>(){
     var schedules = mutableListOf<User.Schedule>()
@@ -24,6 +28,7 @@ class HomeScheduleAdapter() : RecyclerView.Adapter<HomeScheduleAdapter.ViewHolde
         val timeView=binding.scheduleTime
         val complete = binding.completeBtn
         val line=binding.line
+        var catNum :Long = 0
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -34,6 +39,7 @@ class HomeScheduleAdapter() : RecyclerView.Adapter<HomeScheduleAdapter.ViewHolde
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item=schedules[position]
         val scheduleTime : String
+        var catList  = mutableListOf<Long>()
 
         holder.titleView.text=item.title
         if(item.end.isEmpty()){
@@ -44,22 +50,34 @@ class HomeScheduleAdapter() : RecyclerView.Adapter<HomeScheduleAdapter.ViewHolde
         }
         holder.timeView.text=scheduleTime
         Log.i("dd",schedules.toString())
+
         holder.complete.setOnClickListener{
-            Log.d("btn",item.category)
-            Log.d("btn",item.title)
-            Log.d("btn",item.date)
             if (!item.success.toBoolean()) {
-                var path =""
-                var catNum :Long
+                var catNum : Long = 0
+                val fbCat = Firebase.database.getReference("User").child(FBAuth.getUid()).child("category")
+                fbCat.addListenerForSingleValueEvent(object : ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        for (ds in snapshot.children) {
+                            if (ds.key.equals(item.category)) {
+                                catNum = ds.value.toString().toLong()+1
+                                Log.d("catNum","$catNum")
+                                fbCat.child("${item.category}").setValue(catNum)
+                            }
+                        }
+                    }
+                    override fun onCancelled(error: DatabaseError) {
+                        TODO("Not yet implemented")
+                    }
+                })
+//                fbCat.child("${item.category}").setValue(catNum)
+
                 val fbSchedules = Firebase.database.getReference("User").child(FBAuth.getUid()).child("schedule").child(item.date)
-                fbSchedules.orderByChild("start").addValueEventListener(object :
+                fbSchedules.orderByChild("start").addListenerForSingleValueEvent(object :
                     ValueEventListener {
                     override fun onDataChange(snapshot: DataSnapshot) {
                         for (ds in snapshot.children) {
                             if (ds.child("title").value.toString().equals(item.title)) {
-                                Log.d("check",ds.toString())
-                                path = ds.key.toString()
-                                Log.d("check",path)
+                                var path = ds.key.toString()
                                 fbSchedules.child(path).child("success").setValue(true.toString())
                             }
                         }
@@ -68,6 +86,9 @@ class HomeScheduleAdapter() : RecyclerView.Adapter<HomeScheduleAdapter.ViewHolde
                         Log.e("database", "database failed")
                     }
                 })
+            }
+            else{
+                Log.d("complete","이미 완료된 일과입니다.")
             }
         }
         if(position==schedules.size-1){
